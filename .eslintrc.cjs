e583dd958960dad3411a9cfb672f4fff75a552d9
dandocan <vilcu.dan.cristian@gmail.com>
2024-06-02 02:02:54 +0200

    Remove no any eslint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -30,6 +30,7 @@ const config = {
     "@typescript-eslint/no-unsafe-member-access": "off",
     "@typescript-eslint/no-unsafe-argument": "off",
     "@typescript-eslint/no-redundant-type-constituents": "off",
+    "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-misused-promises": [
       2,
       {
